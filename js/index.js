0e24f07bc0c0a4e4c8b0b566b0677f38b0a758de
zyad87 <zyadt871@gmail.com>
2023-10-03 03:30:58 +0300

    اللمسات الأخيرة

--- js/index.js
+++ js/index.js
@@ -52,3 +52,66 @@ window.addEventListener('load', () => {
         body.classList.add('dark-mode');
     }
 });
+
+
+let counting = false;
+
+// تابع لزيادة العداد
+function increaseCount(targetElement, targetValue, increment, interval) {
+    let currentCount = 0;
+    const element = document.getElementById(targetElement);
+
+    const updateCount = () => {
+        if (currentCount < targetValue && counting) {
+            currentCount += increment;
+            element.textContent = currentCount;
+            setTimeout(updateCount, interval);
+        } else {
+            element.textContent = targetValue;
+        }
+    };
+
+    updateCount();
+}
+
+// تفعيل زيادة الأرقام عند الوصول إلى القسم
+window.addEventListener('scroll', () => {
+    const section = document.getElementById('statistics');
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    const scrollPosition = window.scrollY;
+    const triggerPoint = sectionTop + sectionHeight - window.innerHeight;
+
+    if (scrollPosition >= triggerPoint && !counting) {
+        counting = true; // تم تفعيل العداد
+        // زيادة الأرقام هنا
+        increaseCount('website-count', 30, 1, 90); // زيادة العداد الأول
+        increaseCount('app-count', 170, 1, 20); // زيادة العداد الثاني
+        increaseCount('user-count', 15, 1, 150); // زيادة العداد الثالث
+        increaseCount('video-count', 100, 1, 30); // زيادة العداد الرابع
+    }
+});
+
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+  
+        form.classList.add('was-validated')
+      }, false)
+    })
+  })()
+
+
+  document.getElementById('fullYear').innerHTML = new Date().getFullYear();
